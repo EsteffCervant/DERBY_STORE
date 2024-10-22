eb9cee63ef7adc5dbd6a938bbdaa5c82bb6a7370
cervantestefania@gmail.com <cervantestefania@gmail.com>
2024-10-22 11:29:35 -0600

    prueba botones nav

--- START/urls.py
+++ START/urls.py
@@ -5,13 +5,14 @@ from START.views import (
     template1, 
     template2, 
     template3,
-    crear_patines,
+    crear_patines
 )
+#app_name = 'START'
 
 urlpatterns = [
     path('vista/', vista),
     path('', inicio),
-    path('deportes/', template1),
+    path('deportes/', template1, name='deportes'),
     path('llantas-accesorios/', template2),
     path('compra/', template3),
     path('crear-patines/<str:Deporte>/<str:Marca>/<int:Talla>/', crear_patines)
